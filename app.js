0f6dcdd064a6c8e59d0ddfcc9919ced2eaca85b5
Debraj1723 <debraj.basak23@gmail.com>
2024-12-10 00:09:40 +0530

    Episode-2

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 const heading = React.createElement(
   "h1",
   { id: "heading", class: "red" },
   "Hello world from React!"
 );
+
 console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 
 const parent = React.createElement(
