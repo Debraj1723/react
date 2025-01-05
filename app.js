4751f84439c05093e731370f2f4fa4de4271277e
Debraj1723 <debraj.basak23@gmail.com>
2025-01-06 00:04:56 +0530

    episode 3

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = (
-  <h1 className="head" tabIndex="5">
+const TitleComponent = () => (
+  <h1 className="title" tabIndex="5">
     Namaste Debraj Basak
   </h1>
 );
 
+const title = (
+  <h1 className="title" tabIndex="5">
+    Namaste Debraj Basak
+  </h1>
+)
+
+const HeadingComponent = () => (
+  <div className="headingBody">
+    {title}
+    <h1 className="header2">Namaste React component</h1>
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
