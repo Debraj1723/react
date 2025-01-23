0a9f7ac52ee12d134d67c3c6f7b5c951c6673adc
Debraj1723 <debraj.basak23@gmail.com>
2025-01-23 23:37:42 +0530

    learning props

--- app.js
+++ app.js
@@ -1,24 +1,64 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const TitleComponent = () => (
-  <h1 className="title" tabIndex="5">
-    Namaste Debraj Basak
-  </h1>
-);
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://cdn-icons-png.flaticon.com/128/3063/3063822.png"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-const title = (
-  <h1 className="title" tabIndex="5">
-    Namaste Debraj Basak
-  </h1>
-)
+const RestaurantCard = (props) => {
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img src="https://imgs.search.brave.com/fNFN05hZTkTm2daAzXVds3CSha8BlBivoDLfhMJRoXU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTM0/ODc3MzY4My9waG90/by9pbmRpYW4tZm9v/ZC1jaGlja2VuLWJp/cml5YW5pLmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz0xSGpn/SThOS0paNzZqRllm/RHJVLUtubldlWW5q/Y2w1SHZVTnZfeGhL/MjV3PQ" />
+      <h3>{props.name}</h3>
+      <h4>{props.address}</h4>
+    </div>
+  );
+};
 
-const HeadingComponent = () => (
-  <div className="headingBody">
-    {title}
-    <h1 className="header2">Namaste React component</h1>
-  </div>
-);
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="searchContainer">
+        <input placeholder="" />
+      </div>
+      <div className="res-container">
+        <RestaurantCard
+          name="Meghna foods"
+          address="11/21 Bashonti Chatterjee lane"
+        />
+        <RestaurantCard
+          name="KFC restaurant"
+          address="11/21 Bashonti Chatterjee lane"
+        />
+      </div>
+    </div>
+  );
+};
 
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
